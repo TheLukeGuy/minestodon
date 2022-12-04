da4f4707607b04efe963563a6dda5aa9cc254003
Luke Chambers <consolelogluke@gmail.com>
2022-12-04 14:56:55 -0500

    Send the packet length before the body

--- src/mc.rs
+++ src/mc.rs
@@ -1,3 +1,4 @@
+use crate::mc::packet_io::PacketWriteExt;
 use anyhow::{Context, Result};
 use std::io::Write;
 use std::net::TcpStream;
@@ -12,8 +13,17 @@ impl Connection {
     pub fn send_packet(&mut self, write: impl FnOnce(&mut Vec<u8>)) -> Result<()> {
         let mut buf = Vec::with_capacity(1024);
         write(&mut buf);
+
+        let len = buf
+            .len()
+            .try_into()
+            .context("the packet length doesn't fit in an i32")?;
+        self.stream
+            .write_var::<i32>(len)
+            .context("failed to write the packet length")?;
+
         self.stream
             .write_all(&buf)
-            .context("failed to send a packet to the client")
+            .context("failed to write the packet body")
     }
 }
