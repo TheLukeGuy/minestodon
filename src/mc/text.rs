cf8d395c0fa9a80e38517f4184215b48eeaa6372
Luke Chambers <consolelogluke@gmail.com>
2022-12-17 21:05:42 -0500

    Slightly simplify the text macro

--- src/mc/text.rs
+++ src/mc/text.rs
@@ -390,6 +390,6 @@ pub enum JsonStringType {
 macro_rules! text {
     ($($arg:tt)*) => {{
         let formatted = ::std::format!($($arg)*);
-        <$crate::mc::text::Text as ::std::convert::From<::std::string::String>>::from(formatted)
+        $crate::mc::text::Text::String(formatted)
     }};
 }
